0026c5d8a29e9bf25d103177d6bbf1ad7513d98b
adimancifi <official.cifirecms@gmail.com>
2020-02-16 09:55:59 +0700

    Update index.php

--- index.php
+++ index.php
@@ -23,82 +23,17 @@
  * SOFTWARE.
  * 
  * @package	CiFireCMS
- * @author	Adiman
+ * @author	Alweak
  * @license	https://opensource.org/licenses/MIT	MIT License
  * @link	https://www.alweak.com
- * @since	Version 2.0.0
- * @filesource
 */
 
-/**
- * CodeIgniter
- *
- * An open source application development framework for PHP
- *
- * This content is released under the MIT License (MIT)
- *
- * Copyright (c) 2014 - 2019, British Columbia Institute of Technology
- *
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
- *
- * The above copyright notice and this permission notice shall be included in
- * all copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
- * THE SOFTWARE.
- *
- * @package	CodeIgniter
- * @author	EllisLab Dev Team
- * @copyright	Copyright (c) 2008 - 2014, EllisLab, Inc. (https://ellislab.com/)
- * @copyright	Copyright (c) 2014 - 2019, British Columbia Institute of Technology (https://bcit.ca/)
- * @license	https://opensource.org/licenses/MIT	MIT License
- * @link	https://codeigniter.com
- * @since	Version 1.0.0
- * @filesource
- */
-
-/*
- *---------------------------------------------------------------
- * APPLICATION ENVIRONMENT
- *---------------------------------------------------------------
- *
- * You can load different configurations depending on your
- * current environment. Setting the environment also influences
- * things like logging and error reporting.
- *
- * This can be set to anything, but default usage is:
- *
- *     development
- *     testing
- *     production
- *
- * NOTE: If you change these, also change the error_reporting() code below
-*/
 define('ENVIRONMENT', isset($_SERVER['CI_ENV']) ? $_SERVER['CI_ENV'] : 'development');
 
-/*
- *---------------------------------------------------------------
- * ERROR REPORTING
- *---------------------------------------------------------------
- *
- * Different environments will require different levels of error reporting.
- * By default development will show errors but testing and live will hide them.
-*/
 switch (ENVIRONMENT)
 {
 	case 'development':
 		error_reporting(-1);
-		error_reporting(E_ALL ^ E_DEPRECATED);
 		ini_set('display_errors', 1);
 	break;
 
@@ -121,128 +56,11 @@ switch (ENVIRONMENT)
 		exit(1); // EXIT_ERROR
 }
 
-/*
- *---------------------------------------------------------------
- * SYSTEM DIRECTORY NAME
- *---------------------------------------------------------------
- *
- * This variable must contain the name of your "system" directory.
- * Set the path if it is not in the same directory as this file.
-*/
-$system_path = 'l-system';
+$system_path        = 'l-system';
+$application_folder = 'l-install/app';
+$view_folder        = '';
 
-/*
- *---------------------------------------------------------------
- * APPLICATION DIRECTORY NAME
- *---------------------------------------------------------------
- *
- * If you want this front controller to use a different "application"
- * directory than the default one you can set its name here. The directory
- * can also be renamed or relocated anywhere on your server. If you do,
- * use an absolute (full) server path.
- * For more info please see the user guide:
- *
- * https://codeigniter.com/user_guide/general/managing_apps.html
- *
- * NO TRAILING SLASH!
-*/
-$application_folder = 'l-app';
-
-/*
- *---------------------------------------------------------------
- * VIEW DIRECTORY NAME
- *---------------------------------------------------------------
- *
- * If you want to move the view directory out of the application
- * directory, set the path to it here. The directory can be renamed
- * and relocated anywhere on your server. If blank, it will default
- * to the standard location inside your application directory.
- * If you do move this, use an absolute (full) server path.
- *
- * NO TRAILING SLASH!
-*/
-$view_folder = '';
-
-/*
- *---------------------------------------------------------------
- * ADMINISTRATOR DIRECTORY NAME
- *---------------------------------------------------------------
-*/
-$admin_folder = 'l-admin';
-
-/*
- *---------------------------------------------------------------
- * WEB DIRECTORY NAME
- *---------------------------------------------------------------
-*/
-$web_folder = 'l-web';
-
-/*
- *---------------------------------------------------------------
- * CONTENT DIRECTORY NAME
- *---------------------------------------------------------------
-*/
-$content_folder = 'l-content';
-
-/*
- * --------------------------------------------------------------------
- * DEFAULT CONTROLLER
- * --------------------------------------------------------------------
- *
- * Normally you will set your default controller in the routes.php file.
- * You can, however, force a custom routing by hard-coding a
- * specific controller class/function here. For most applications, you
- * WILL NOT set your routing here, but it's an option for those
- * special instances where you might want to override the standard
- * routing in a specific front controller that shares a common CI installation.
- *
- * IMPORTANT: If you set the routing here, NO OTHER controller will be
- * callable. In essence, this preference limits your application to ONE
- * specific controller. Leave the function name blank if you need
- * to call functions dynamically via the URI.
- *
- * Un-comment the $routing array below to use this feature
-*/
-	// The directory name, relative to the "controllers" directory.  Leave blank
-	// if your controller is not in a sub-directory within the "controllers" one
-	// $routing['directory'] = '';
-
-	// The controller class file name.  Example:  mycontroller
-	// $routing['controller'] = '';
-
-	// The controller function you wish to be called.
-	// $routing['function']	= '';
-
-
-/*
- * -------------------------------------------------------------------
- *  CUSTOM CONFIG VALUES
- * -------------------------------------------------------------------
- *
- * The $assign_to_config array below will be passed dynamically to the
- * config class when initialized. This allows you to set custom config
- * items or override any default config values found in the config.php file.
- * This can be handy as it permits you to share one application between
- * multiple front controller files, with each file containing different
- * config values.
- *
- * Un-comment the $assign_to_config array below to use this feature
-*/
-	// $assign_to_config['name_of_config_item'] = 'value of config item';
-
-
-
-// --------------------------------------------------------------------
-// END OF USER CONFIGURABLE SETTINGS.  DO NOT EDIT BELOW THIS LINE
-// --------------------------------------------------------------------
-
-/*
- * ---------------------------------------------------------------
- *  Resolve the system path for increased reliability
- * ---------------------------------------------------------------
-*/
-
-// Set the current directory correctly for CLI requests.
+// Set the current directory correctly for CLI requests
 if (defined('STDIN'))
 {
 	chdir(dirname(__FILE__));
@@ -254,7 +72,7 @@ if (($_temp = realpath($system_path)) !== FALSE)
 }
 else
 {
-	// Ensure there's a trailing slash.
+	// Ensure there's a trailing slash
 	$system_path = strtr(
 		rtrim($system_path, '/\\'),
 		'/\\',
@@ -270,19 +88,19 @@ if ( ! is_dir($system_path))
 	exit(3); // EXIT_CONFIG
 }
 
-// The name of THIS file.
+// The name of THIS file
 define('SELF', pathinfo(__FILE__, PATHINFO_BASENAME));
 
-// Path to the system directory.
+// Path to the system directory
 define('BASEPATH', $system_path);
 
-// Path to the front controller (this file) directory.
+// Path to the front controller (this file) directory
 define('FCPATH', dirname(__FILE__).DIRECTORY_SEPARATOR);
 
-// Name of the "system" directory.
+// Name of the "system" directory
 define('SYSDIR', basename(BASEPATH));
 
-// The path to the "application" directory.
+// The path to the "application" directory
 if (is_dir($application_folder))
 {
 	if (($_temp = realpath($application_folder)) !== FALSE)
@@ -315,7 +133,7 @@ else
 
 define('APPPATH', $application_folder.DIRECTORY_SEPARATOR);
 
-// The path to the "views" directory.
+// The path to the "views" directory
 if ( ! isset($view_folder[0]) && is_dir(APPPATH.'views'.DIRECTORY_SEPARATOR))
 {
 	$view_folder = APPPATH.'views';
@@ -352,17 +170,4 @@ else
 
 define('VIEWPATH', $view_folder.DIRECTORY_SEPARATOR);
 
-// Custom path.
-define('CONTENTPATH', $content_folder.DIRECTORY_SEPARATOR);
-define('FCONTENT', $content_folder);
-define('FADMIN', $admin_folder);
-define('FWEB', $web_folder);
-
-/*
- * --------------------------------------------------------------------
- * LOAD THE BOOTSTRAP FILE
- * --------------------------------------------------------------------
- *
- * And away we go...
-*/
 require_once BASEPATH.'core/CodeIgniter.php';
