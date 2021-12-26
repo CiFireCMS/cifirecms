e2dba66eb12170ff24f1208e74507aade42ae970
adimancifi <46333102+adimancifi@users.noreply.github.com>
2021-12-27 02:52:43 +0800

    Update auth_footer.php

--- app/views/backend/auth_footer.php
+++ app/views/backend/auth_footer.php
@@ -1,13 +1,5 @@
 <?php defined('BASEPATH') OR exit('No direct script access allowed');?>
+	<script src="<?=site_url('plugins/jquery/jquery.min.js');?>"></script>
 	<script src="<?=site_url('plugins/bootstrap/js/bootstrap.min.js');?>"></script>
-	<script src="<?=site_url('plugins/pace/pace.min.js');?>"></script>
-	<script src="<?=site_url('plugins/feather/feather.min.js');?>"></script>
-	<script src="<?=site_url('plugins/slimscroll/jquery.slimscroll.min.js');?>"></script>
-	<script src="<?=site_url('plugins/perfect-scrollbar/perfect-scrollbar.js');?>"></script>
-	<script src="<?=site_url('plugins/bootstrap-select/bootstrap-select.min.js');?>"></script>
-	<script src="<?=site_url('plugins/select2/select2.min.js');?>"></script>
-	<script src="<?=site_url('plugins/fancybox-2.1.7/jquery.fancybox.pack.js');?>"></script>
-	<script src="<?=site_url('plugins/fancybox-2.1.7/jquery.fancybox-media.js');?>"></script>
-	<script src="<?=site_url('themes/backend/js/app.js');?>"></script>
 </body>
 </html>
